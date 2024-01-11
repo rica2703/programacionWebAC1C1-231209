8468cdc190528cb40cb98feddc8b954891bb965b
rica2703 <rica2703@gmail.com>
2024-01-10 21:12:03 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './main.css'
+
+function App() {
+  
+
+  return (
+    <>
+       <div className='divPadre'>
+
+<div className="rojo">
+    <div className="azul">
+        <div className="verde">
+
+        </div>
+    </div>
+</div>
+</div>
+    </>
+  )
+}
+
+export default App
